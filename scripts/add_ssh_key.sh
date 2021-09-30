0c190ff3b7e314dc4377e34ffbe16e1b08ac01b9
BMarcin <m1.borzymowski@gmail.com>
2021-09-30 21:47:08 +0200

    fix ssh key

--- scripts/add_ssh_key.sh
+++ scripts/add_ssh_key.sh
@@ -4,7 +4,7 @@ mkdir -p ~/.ssh
 chmod 700 ~/.ssh
 cp ./scripts/ssh_config ~/.ssh/config
 
-echo $GONITO_PRIVATE_SSH > ~/.ssh/gonito
+echo $GONITO_PRIVATE_SSH | base64 -d > ~/.ssh/gonito
 chmod 600 ~/.ssh/gonito
 
 ssh-keyscan -H gonito.net >> ~/.ssh/known_hosts
