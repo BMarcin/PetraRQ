5899297c75b44520588ece110930bd9d985e4470
BMarcin <m1.borzymowski@gmail.com>
2021-09-30 21:55:34 +0200

    try to commit to the repo

--- scripts/build_gonito_main_repo.sh
+++ scripts/build_gonito_main_repo.sh
@@ -4,7 +4,7 @@ mkdir -p /home/runner/work/PetraRQ/main_repo/
 echo "Created main repo"
 
 cd /home/runner/work/PetraRQ/main_repo/
-#git clone ssh://gitolite@gonito.net/eur-lex-documents
+git clone ssh://gitolite@gonito.net/eur-lex-documents
 git init
 
 cp /home/runner/work/PetraRQ/PetraRQ/README.md .
@@ -18,7 +18,7 @@ cp /home/runner/work/PetraRQ/PetraRQ/data/dev/* ./dev-0/
 cp /home/runner/work/PetraRQ/PetraRQ/data/test/* ./test-A/
 cp /home/runner/work/PetraRQ/PetraRQ/data/train/* ./train/
 
-git add .
-git commit -m "update files"
-git remote add origin ssh://gitolite@gonito.net/eur-lex-documents-dont-peek
+#git add .
+git commit -am "update files"
+#git remote add origin ssh://gitolite@gonito.net/eur-lex-documents-dont-peek
 git push origin master
