f2a42cf3d05bd747e9305639b7113ac232275042
BMarcin <m1.borzymowski@gmail.com>
2021-09-30 22:07:34 +0200

    push to the main repo

--- scripts/build_gonito_main_repo.sh
+++ scripts/build_gonito_main_repo.sh
@@ -4,9 +4,9 @@ mkdir -p /home/runner/work/PetraRQ/main_repo/
 echo "Created main repo"
 
 cd /home/runner/work/PetraRQ/main_repo/
-git clone ssh://gitolite@gonito.net/eur-lex-documents-dont-peek
-#git init
-cd eur-lex-documents-dont-peek
+#git clone ssh://gitolite@gonito.net/eur-lex-documents-dont-peek
+#cd eur-lex-documents-dont-peek
+git init
 
 cp /home/runner/work/PetraRQ/PetraRQ/README.md .
 cp /home/runner/work/PetraRQ/PetraRQ/config.txt .
@@ -18,8 +18,9 @@ mkdir -p ./test-A
 cp /home/runner/work/PetraRQ/PetraRQ/data/dev/* ./dev-0/
 cp /home/runner/work/PetraRQ/PetraRQ/data/test/* ./test-A/
 cp /home/runner/work/PetraRQ/PetraRQ/data/train/* ./train/
+rm ./test-A/expected.tsv
 
-#git add .
+git add .
 git commit -am "update files"
-#git remote add origin ssh://gitolite@gonito.net/eur-lex-documents-dont-peek
+git remote add origin ssh://gitolite@gonito.net/eur-lex-documents
 git push origin master
