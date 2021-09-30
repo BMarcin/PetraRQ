c328b50e20c4f7091800198591cbf3936f888fc3
BMarcin <m1.borzymowski@gmail.com>
2021-09-30 21:37:05 +0200

    fix git and ssh keys

--- scripts/add_ssh_key.sh
+++ scripts/add_ssh_key.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 mkdir -p ~/.ssh
+chmod 700 ~/.ssh
 cp ./scripts/ssh_config ~/.ssh/config
 
 echo $GONITO_PRIVATE_SSH > ~/.ssh/gonito
 chmod 600 ~/.ssh/gonito
+
+ssh-keyscan -H gonito.net >> ~/.ssh/known_hosts
+
+git config --global user.email "m1.borzymowski@gmail.com"
+git config --global user.name "Marcin Borzymowski"
