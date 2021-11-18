ea9b5d3c7fb573cb66de1fa8d92e8520b947a20a
BMarcin <m1.borzymowski@gmail.com>
2021-11-18 19:01:35 +0100

    fixes for ds splitter & 2x quadro gpu retraining, big roberta

--- src/split_dataset.py
+++ src/split_dataset.py
@@ -3,6 +3,7 @@ import json
 import logging
 from pathlib import Path
 import yaml
+import pandas as pd
 
 from tqdm.auto import tqdm
 
@@ -16,9 +17,10 @@ def save_ds_part(items: list, in_filename: Path, expected_filename: Path, label_
             csv_expected_writer = csv.writer(expected_file, delimiter='\t', quoting=csv.QUOTE_MINIMAL)
 
             for item in tqdm(items, desc='Saving {} and {}'.format(str(in_filename), str(expected_filename))):
-                csv_in_writer.writerow([item['content'].replace("\n", " ")])
-                buckets = [label_replacement_list[lab.lower().strip()] for lab in item['buckets']]
-                csv_expected_writer.writerow([' '.join(buckets)])
+                if len(item['content'].replace("\n", " ").replace(" ", "")) >= 3:
+                    csv_in_writer.writerow([item['content'].replace("\n", " ")])
+                    buckets = [label_replacement_list[lab.lower().strip()] for lab in item['buckets']]
+                    csv_expected_writer.writerow([' '.join(buckets)])
 
 
 if __name__ == '__main__':
@@ -55,8 +57,8 @@ if __name__ == '__main__':
         save_ds_part(splitted_items['test'], Path('./data/test/in.tsv'), Path('./data/test/expected.tsv'), labels)
         save_ds_part(splitted_items['train'], Path('./data/train/in.tsv'), Path('./data/train/expected.tsv'), labels)
 
-        assert len(open("./data/dev/in.tsv").readlines()) == len(open("./data/dev/expected.tsv").readlines()), 'DEV dataset lines count missmatch'
-        assert len(open("./data/test/in.tsv").readlines()) == len(open("./data/test/expected.tsv").readlines()), 'TEST dataset lines count missmatch'
-        assert len(open("./data/train/in.tsv").readlines()) == len(open("./data/train/expected.tsv").readlines()), 'TRAIN dataset lines count missmatch'
+        assert len(pd.read_csv("./data/dev/in.tsv")) == len(pd.read_csv("./data/dev/expected.tsv")), 'DEV dataset lines count missmatch'
+        assert len(pd.read_csv("./data/test/in.tsv")) == len(pd.read_csv("./data/test/expected.tsv")), 'TEST dataset lines count missmatch'
+        assert len(pd.read_csv("./data/train/in.tsv")) == len(pd.read_csv("./data/train/expected.tsv")), 'TRAIN dataset lines count missmatch'
     else:
         logging.error("File {} does not exists".format(str(ds_path)))
