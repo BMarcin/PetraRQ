ddf9e82a21afc30479b53bdc769fc4855f043320
BMarcin <m1.borzymowski@gmail.com>
2021-12-05 11:35:11 +0100

    fix building gonito structure

--- scripts/build_gonito_repos.sh
+++ scripts/build_gonito_repos.sh
@@ -24,12 +24,48 @@ mkdir -p ./train
 mkdir -p ./dev-0
 mkdir -p ./test-A
 
+if [ -f "./train/in.tsv.gz" ]; then
+  rm ./train/in.tsv.gz
+fi
+
+if [ -f "./train/expected.tsv.gz" ]; then
+  rm ./train/expected.tsv.gz
+fi
+
+if [ -f "./dev-0/in.tsv.gz" ]; then
+  rm ./dev-0/in.tsv.gz
+fi
+
+if [ -f "./dev-0/expected.tsv.gz" ]; then
+  rm ./dev-0/expected.tsv.gz
+fi
+
+if [ -f "./dev-0/out.tsv.gz" ]; then
+  rm ./dev-0/out.tsv.gz
+fi
+
+if [ -f "./test-A/in.tsv.gz" ]; then
+  rm ./test-A/in.tsv.gz
+fi
+
+if [ -f "./test-A/expected.tsv.gz" ]; then
+  rm ./test-A/expected.tsv.gz
+fi
+
+if [ -f "./test-A/out.tsv.gz" ]; then
+  rm ./test-A/out.tsv.gz
+fi
+
 cp /home/runner/work/PetraRQ/PetraRQ/data/dev/* ./dev-0/
 cp /home/runner/work/PetraRQ/PetraRQ/data/test/* ./test-A/
 cp /home/runner/work/PetraRQ/PetraRQ/data/train/* ./train/
 mv /home/runner/work/PetraRQ/PetraRQ/data/in-header.tsv ./
 mv /home/runner/work/PetraRQ/PetraRQ/data/out-header.tsv ./
 #rm ./test-A/expected.tsv
+
+rm ./dev-0/lm.txt
+rm ./test-A/lm.txt
+rm ./train/lm.txt
 
 gzip ./train/in.tsv
 gzip ./train/expected.tsv
