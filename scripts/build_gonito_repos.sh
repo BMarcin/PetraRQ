7a0dad7e364eb252b21710f648fc8dfe6d3a929c
BMarcin <m1.borzymowski@gmail.com>
2021-12-08 00:04:40 +0100

    test pushing changes to gonito (dont peek)

--- scripts/build_gonito_repos.sh
+++ scripts/build_gonito_repos.sh
@@ -91,18 +91,17 @@ git status
 git commit -m "$COMMIT_MESSAGE"
 git push -f origin "$BRANCH_NAME"
 
-#git remote rm origin
-#git remote add origin ssh://gitolite@gonito.net/eur-lex-documents-dont-peek
-#git pull
-#
-#mv ../expected.tsv.gz ./test-A/expected.tsv.gz
-#
-#tree
-#
-#git add ./test-A/expected.tsv.gz
-#git status
-#git commit -m "$COMMIT_MESSAGE"
-#git push origin "$BRANCH_NAME"
+git remote rm origin
+git remote add origin ssh://gitolite@gonito.net/eur-lex-documents-dont-peek
+
+mv ../expected.tsv.gz ./test-A/expected.tsv.gz
+
+tree
+
+git add ./test-A/expected.tsv.gz
+git status
+git commit -m "$COMMIT_MESSAGE"
+git push -f origin "$BRANCH_NAME"
 
 
 #git commit -am $COMMIT_MESSAGE
