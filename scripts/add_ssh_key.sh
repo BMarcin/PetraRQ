7df44c0e0978a9fd5af6ec2df8242f99970bded1
BMarcin <m1.borzymowski@gmail.com>
2022-03-17 23:16:41 +0100

    debug pipeline

--- scripts/add_ssh_key.sh
+++ scripts/add_ssh_key.sh
@@ -4,10 +4,10 @@ mkdir -p ~/.ssh
 chmod 700 ~/.ssh
 cp ./scripts/ssh_config ~/.ssh/config
 
-#ssh-keyscan -H gonito.net > ~/.ssh/known_hosts
-
 echo $GONITO_PRIVATE_SSH | base64 -d > ~/.ssh/gonito
 chmod 600 ~/.ssh/gonito
+
+ssh-keyscan -H gonito.net >> ~/.ssh/known_hosts
 
 git config --global user.email "m1.borzymowski@gmail.com"
 git config --global user.name "Marcin Borzymowski"
