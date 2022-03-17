37efc4d00e99b0e5e359dafb8dcf169a5aa3139a
BMarcin <m1.borzymowski@gmail.com>
2022-03-17 23:13:04 +0100

    debug pipeline

--- scripts/add_ssh_key.sh
+++ scripts/add_ssh_key.sh
@@ -4,8 +4,7 @@ mkdir -p ~/.ssh
 chmod 700 ~/.ssh
 cp ./scripts/ssh_config ~/.ssh/config
 
-ssh-keyscan -H gonito.net
-ssh-keyscan -H gonito.net > ~/.ssh/known_hosts
+#ssh-keyscan -H gonito.net > ~/.ssh/known_hosts
 
 echo $GONITO_PRIVATE_SSH | base64 -d > ~/.ssh/gonito
 chmod 600 ~/.ssh/gonito
