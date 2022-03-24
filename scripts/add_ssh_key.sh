5e467adee03c5185771429205c005e9c2657afbb
BMarcin <m1.borzymowski@gmail.com>
2022-03-24 22:32:14 +0100

    debug ssh keys

--- scripts/add_ssh_key.sh
+++ scripts/add_ssh_key.sh
@@ -3,16 +3,16 @@
 whoami
 pwd
 
-mkdir -p ~/.ssh
-chmod 700 ~/.ssh
-cp ./scripts/ssh_config ~/.ssh/config
+#mkdir -p ~/.ssh
+#chmod 700 ~/.ssh
+#cp ./scripts/ssh_config ~/.ssh/config
 
 # fix for self hosted
 mkdir -p /root/.ssh
 chmod 700 /root/.ssh
 cp ./scripts/ssh_config /root/.ssh/config
 
-ssh-keyscan -H gonito.net >> ~/.ssh/known_hosts
+#ssh-keyscan -H gonito.net >> ~/.ssh/known_hosts
 # fix for self hosted
 ssh-keyscan -H gonito.net >> /root/.ssh/known_hosts
 
