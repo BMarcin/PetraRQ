2242964e9016167ec27e6539bb8bf68caa9438f8
BMarcin <m1.borzymowski@gmail.com>
2022-03-24 22:09:37 +0100

    fix for selfhosted runner

--- scripts/add_ssh_key.sh
+++ scripts/add_ssh_key.sh
@@ -7,10 +7,14 @@ mkdir -p ~/.ssh
 chmod 700 ~/.ssh
 cp ./scripts/ssh_config ~/.ssh/config
 
-echo $GONITO_PRIVATE_SSH | base64 -d > ~/.ssh/gonito
-chmod 600 ~/.ssh/gonito
+# fix for self hosted
+mkdir -p /root/.ssh
+chmod 700 /root/.ssh
+cp ./scripts/ssh_config /root/.ssh/config
 
 ssh-keyscan -H gonito.net >> ~/.ssh/known_hosts
+# fix for self hosted
+ssh-keyscan -H gonito.net >> /root/.ssh/known_hosts
 
 git config --global user.email "m1.borzymowski@gmail.com"
 git config --global user.name "Marcin Borzymowski"
