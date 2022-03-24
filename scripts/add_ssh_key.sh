9ac369a268eed5265c0688a58d60f7ac5099140d
BMarcin <m1.borzymowski@gmail.com>
2022-03-24 22:38:04 +0100

    debug ssh keys

--- scripts/add_ssh_key.sh
+++ scripts/add_ssh_key.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
-whoami
-pwd
-
-#mkdir -p ~/.ssh
-#chmod 700 ~/.ssh
-#cp ./scripts/ssh_config ~/.ssh/config
-
-# fix for self hosted
+mkdir -p ~/.ssh
+chmod 700 ~/.ssh
+cp ./scripts/ssh_config ~/.ssh/config
 mkdir -p /root/.ssh
 chmod 700 /root/.ssh
 cp ./scripts/ssh_config /root/.ssh/config
 
-#ssh-keyscan -H gonito.net >> ~/.ssh/known_hosts
-# fix for self hosted
+echo $GONITO_PRIVATE_SSH | base64 -d > ~/.ssh/gonito
+echo $GONITO_PRIVATE_SSH | base64 -d > /root/.ssh/gonito
+chmod 600 ~/.ssh/gonito
+chmod 600 /root/.ssh/gonito
+
+ssh-keyscan -H gonito.net >> ~/.ssh/known_hosts
 ssh-keyscan -H gonito.net >> /root/.ssh/known_hosts
 
 git config --global user.email "m1.borzymowski@gmail.com"
