c0302ead93f6f9d3b28e69b11106d3e18751882e
BMarcin <m1.borzymowski@gmail.com>
2022-04-03 16:24:18 +0200

    fix for selfhosted

--- scripts/build_gonito_submission_repo.sh
+++ scripts/build_gonito_submission_repo.sh
@@ -21,14 +21,14 @@ git switch -c "$BRANCH_NAME"
 mkdir -p ./src
 mkdir -p ./docker
 
-cp /app/README.md .
-cp /app/config.txt .
-cp /app/.gitignore .
-cp /app/gonito.yaml .
-cp /app/scripts/train.sh .
-cp /app/scripts/predict.sh .
-cp -r /app/src/* ./src/
-cp -r /app/docker/* ./docker/
+cp /app2/README.md .
+cp /app2/config.txt .
+cp /app2/.gitignore .
+cp /app2/gonito.yaml .
+cp /app2/scripts/train.sh .
+cp /app2/scripts/predict.sh .
+cp -r /app2/src/* ./src/
+cp -r /app2/docker/* ./docker/
 
 chmod +x ./train.sh
 chmod +x ./predict.sh
@@ -75,24 +75,24 @@ if [ -f "./test-A/out.tsv" ]; then
   rm ./test-A/out.tsv
 fi
 
-tr -d '\015' </app/data/dev/in.tsv >./dev-0/in.tsv
-tr -d '\015' </app/data/dev/expected.tsv >./dev-0/expected.tsv
-tr -d '\015' </app/data/dev/out.tsv >./dev-0/out.tsv
+tr -d '\015' </app2/data/dev/in.tsv >./dev-0/in.tsv
+tr -d '\015' </app2/data/dev/expected.tsv >./dev-0/expected.tsv
+tr -d '\015' </app2/data/dev/out.tsv >./dev-0/out.tsv
 
-tr -d '\015' </app/data/test/in.tsv >./test-A/in.tsv
-tr -d '\015' </app/data/test/out.tsv >./test-A/out.tsv
+tr -d '\015' </app2/data/test/in.tsv >./test-A/in.tsv
+tr -d '\015' </app2/data/test/out.tsv >./test-A/out.tsv
 
-tr -d '\015' </app/data/train/in.tsv >./train/in.tsv
-tr -d '\015' </app/data/train/expected.tsv >./train/expected.tsv
+tr -d '\015' </app2/data/train/in.tsv >./train/in.tsv
+tr -d '\015' </app2/data/train/expected.tsv >./train/expected.tsv
 
 
-mv /app/data/in-header.tsv ./
-mv /app/data/out-header.tsv ./
+mv /app2/data/in-header.tsv ./
+mv /app2/data/out-header.tsv ./
 
 xz ./train/in.tsv
 xz ./test-A/in.tsv
 xz ./dev-0/in.tsv
-/app/geval --validate --expected-directory .
+/app2/geval --validate --expected-directory .
 
 git remote rm origin
 git remote add origin ssh://gitolite@gonito.net/marcinb/eur-lex-documents
