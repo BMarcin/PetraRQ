89cf111d942524d558a2bd5432f7a172ca98eb1a
BMarcin <m1.borzymowski@gmail.com>
2022-04-19 09:32:08 +0200

    disable zipping

--- scripts/build_gonito_submission_repo.sh
+++ scripts/build_gonito_submission_repo.sh
@@ -45,54 +45,54 @@ mkdir -p ./train
 mkdir -p ./dev-0
 mkdir -p ./test-A
 
-if [ -f "./train/in.tsv.xz" ]; then
-  rm ./train/in.tsv.xz
-fi
+#if [ -f "./train/in.tsv.xz" ]; then
+#  rm ./train/in.tsv.xz
+#fi
 
-if [ -f "./train/expected.tsv" ]; then
-  rm ./train/expected.tsv
-fi
+#if [ -f "./train/expected.tsv" ]; then
+#  rm ./train/expected.tsv
+#fi
 
-if [ -f "./dev-0/in.tsv.xz" ]; then
-  rm ./dev-0/in.tsv.xz
-fi
+#if [ -f "./dev-0/in.tsv.xz" ]; then
+#  rm ./dev-0/in.tsv.xz
+#fi
 
-if [ -f "./dev-0/expected.tsv" ]; then
-  rm ./dev-0/expected.tsv
-fi
+#if [ -f "./dev-0/expected.tsv" ]; then
+#  rm ./dev-0/expected.tsv
+#fi
 
 if [ -f "./dev-0/out.tsv.gz" ]; then
   rm ./dev-0/out.tsv.gz
 fi
 
-if [ -f "./test-A/in.tsv.xz" ]; then
-  rm ./test-A/in.tsv.xz
-fi
+#if [ -f "./test-A/in.tsv.xz" ]; then
+#  rm ./test-A/in.tsv.xz
+#fi
 
-if [ -f "./test-A/expected.tsv" ]; then
-  rm ./test-A/expected.tsv
-fi
+#if [ -f "./test-A/expected.tsv" ]; then
+#  rm ./test-A/expected.tsv
+#fi
 
 if [ -f "./test-A/out.tsv" ]; then
   rm ./test-A/out.tsv
 fi
 
-tr -d '\015' </app/data/dev/in.tsv >./dev-0/in.tsv
-tr -d '\015' </app/data/dev/expected.tsv >./dev-0/expected.tsv
-
-tr -d '\015' </app/data/test/in.tsv >./test-A/in.tsv
+#tr -d '\015' </app/data/dev/in.tsv >./dev-0/in.tsv
+#tr -d '\015' </app/data/dev/expected.tsv >./dev-0/expected.tsv
+#
+#tr -d '\015' </app/data/test/in.tsv >./test-A/in.tsv
 tr -d '\015' </app/data/test/out.tsv >./test-A/out.tsv
 
-tr -d '\015' </app/data/train/in.tsv >./train/in.tsv
-tr -d '\015' </app/data/train/expected.tsv >./train/expected.tsv
+#tr -d '\015' </app/data/train/in.tsv >./train/in.tsv
+#tr -d '\015' </app/data/train/expected.tsv >./train/expected.tsv
 
 
 mv /app/data/in-header.tsv ./
 mv /app/data/out-header.tsv ./
 
-xz ./train/in.tsv
-xz ./test-A/in.tsv
-xz ./dev-0/in.tsv
+#xz ./train/in.tsv
+#xz ./test-A/in.tsv
+#xz ./dev-0/in.tsv
 #/app/geval --validate --expected-directory .
 
 tr -d '\015' </app/data/dev/out.tsv >./dev-0/out.tsv
@@ -100,16 +100,12 @@ tr -d '\015' </app/data/dev/out.tsv >./dev-0/out.tsv
 git remote rm origin
 git remote add origin ssh://gitolite@gonito.net/marcinb/eur-lex-documents
 
-git annex forget --force
-
 git add .
-git-annex add ./train/in.tsv.xz
-#chmod 777 ./train/in.tsv.xz
 git status
 echo "before commit"
 git commit -m "$COMMIT_MESSAGE"
 echo "after commit"
 git push -f origin "$BRANCH_NAME"
 echo "after push"
-git-annex sync --content --force --no-pull --push --all
+git-annex sync --no-content --no-pull --push --all
 echo "after sync"
