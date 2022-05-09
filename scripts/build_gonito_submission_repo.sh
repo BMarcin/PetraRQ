ff63cb24902a30c8911d19a14292bc349872b2fb
BMarcin <m1.borzymowski@gmail.com>
2022-05-09 18:17:08 +0200

    try to push to gonito

--- scripts/build_gonito_submission_repo.sh
+++ scripts/build_gonito_submission_repo.sh
@@ -114,8 +114,8 @@ cp /app/data/out-header.tsv ./
 #xz ./dev-0/in.tsv
 #/app/geval --validate --expected-directory .
 
-git-annex add ./train/in.tsv.xz
 git add .
+git-annex add ./train/in.tsv.xz
 git status
 echo "before commit"
 git commit -m "$COMMIT_MESSAGE"
