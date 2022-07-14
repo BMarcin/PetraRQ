b887b60c78b095ab59d15375d0885a9d25d2385c
BMarcin <m1.borzymowski@gmail.com>
2022-07-15 01:07:22 +0200

    fix repro script

--- scripts/build_gonito_submission_repo.sh
+++ scripts/build_gonito_submission_repo.sh
@@ -28,8 +28,8 @@ git-annex sync --content
 
 cd ../eur-lex-documents-marcinb
 
-git switch -c roberta-nn
-git pull origin roberta-nn || true
+git switch -c roberta-nn-10k
+git pull origin roberta-nn-10k || true
 
 # use git annex
 git-annex init
@@ -133,7 +133,7 @@ git status
 echo "before commit"
 git commit -m "$COMMIT_MESSAGE"
 echo "after commit"
-git push -f origin roberta-nn
+git push -f origin roberta-nn-10k
 echo "after push"
 git-annex sync --no-content --no-pull --push --all
 echo "after sync"
