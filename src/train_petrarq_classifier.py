00fc60a8c50800c658d2ca8308f9395dcfe022cc
BMarcin <m1.borzymowski@gmail.com>
2022-09-25 13:23:33 +0200

    change class to xlmroberta

--- src/train_petrarq_classifier.py
+++ src/train_petrarq_classifier.py
@@ -10,7 +10,7 @@ from pytorch_lightning import seed_everything
 from pytorch_lightning.callbacks import ModelCheckpoint, LearningRateMonitor, TQDMProgressBar, EarlyStopping
 from pytorch_lightning.loggers import WandbLogger
 from torch.utils.data import DataLoader
-from transformers import RobertaTokenizerFast, RobertaModel
+from transformers import XLMRobertaTokenizerFast, XLMRobertaModel
 
 from PetraRQ.ClassificationDataset import ClassificationDataset
 from PetraRQ.CollateFunction import coll_fn
@@ -52,7 +52,7 @@ if __name__ == '__main__':
 
 
     logging.info("Loading tokenizer...")
-    tokenizer = RobertaTokenizerFast.from_pretrained("xlm-roberta-base", max_len=512,
+    tokenizer = XLMRobertaTokenizerFast.from_pretrained("xlm-roberta-base", max_len=512,
                                                      use_fast=True)
 
     # Load the data
@@ -149,7 +149,7 @@ if __name__ == '__main__':
 
     # define model
     logging.info("Defining model...")
-    model = RobertaModel.from_pretrained("xlm-roberta-base")
+    model = XLMRobertaModel.from_pretrained("xlm-roberta-base")
     embeds = model.embeddings
 
     petra = PetraRQ(
